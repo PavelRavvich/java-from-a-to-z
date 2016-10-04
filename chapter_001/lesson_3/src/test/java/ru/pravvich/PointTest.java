02b6605b4b337e8296fec1f84afb07f8b14f0b65
Pavel <triodjangopiter@yandex.ru>
2016-10-04 21:25:30 +0300

    add structure

---
+++ chapter_001/lesson_3/src/test/java/ru/pravvich/PointTest.java
@@ -0,0 +1,22 @@
+package ru.pravvich;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PointTest {	
+	@Test
+	public void wthenSetPointThenResultDistanceBetweenPoint() {
+			Point a = new Point(0, 0);
+			Point b = new Point(0, 4);
+			double result = a.distanceTo(b); 						
+			assertThat(result, is(4));
+	
+	}
+	
+	
+
+}
+
+
+
