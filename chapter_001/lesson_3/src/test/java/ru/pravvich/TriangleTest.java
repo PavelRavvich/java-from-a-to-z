57163ea38759eedb3b502733eb6d5e3e26e2c6c8
Pavel <triodjangopiter@yandex.ru>
2016-10-09 08:37:38 +0300

    add chapter_001\lesson_3\src\test\java\ru\pravvich\TriangleTest.java

--- chapter_001/lesson_3/src/test/java/ru/pravvich/TriangleTest.java
+++ chapter_001/lesson_3/src/test/java/ru/pravvich/TriangleTest.java
@@ -7,16 +7,28 @@ import static org.junit.Assert.*;
 public class TriangleTest {	
 	@Test
 	public void wthenSetPointsThenResultAreaTriangle() {
-			Point a = new Point(0,0);
-			Point b = new Point(0,3);
-			Point c = new Point(4,0);
-			
-			Triangle triangle = new Triangle(a, b, c);
-			
-			double result = triangle.area(a, b, c); 						
-			assertThat(result, is(6d));
-	
+		
+		//Вычисление площади для нормального треугольника
+		Point a = new Point(0,0);
+		Point b = new Point(0,3);
+		Point c = new Point(4,0);
+		Triangle triangle = new Triangle(a, b, c);
+		double result = triangle.area(a, b, c); 						
+		assertThat(result, is(6d));
 	}
+	
+	
+	@Test
+    public void whenTheSumOfAnyTwoSidesIsGreaterThanOrEqualToAThirdThenLeaveAMessageTriangleDoesNotExist() {
+
+        //Если сумма двух любых сторон больше или равна третей.
+        Point aUnreal = new Point(1,1);
+        Point bUnreal = new Point(1,2);
+        Point cUnreal = new Point(1,3);
+        Triangle triangleExist = new Triangle(aUnreal, bUnreal, cUnreal);
+        double resultUnreal = triangleExist.area(aUnreal, bUnreal, cUnreal);
+        assertThat(resultUnreal, is(-1D));
+    }
 }
 
 
