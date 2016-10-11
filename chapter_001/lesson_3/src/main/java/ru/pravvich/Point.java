d4da0075eecfe50a0b92055b35104853322d5d5c
Pavel <triodjangopiter@yandex.ru>
2016-10-11 21:25:52 +0300

    add chapter_001/lesson_3/src/main/java/ru/pravvich/Point.java

--- chapter_001/lesson_3/src/main/java/ru/pravvich/Point.java
+++ chapter_001/lesson_3/src/main/java/ru/pravvich/Point.java
@@ -8,21 +8,19 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
-		
+	
+	/*
+	Метод вычисляет расстояние между двумя точками на двухмерной оси координат x,y.
+	Вызывается на одном объекте(точке) и принимает в качестве параметра другой объект(точку).
+	К полям объекта на котором вызывается метод доступаемся через this.поле так как 
+	он у нас основной и на нем и вызывается метод. А к полям принятого объекта доступаемся
+	через его имя которое он получает в сигнатуре(that.поле)
+	*/	
 	public double distanceTo(Point that) {
-		double thatX, thatY, thisX, thisY;
-		
-		thisX = this.x;
-		thisY = this.y;
-		thatX = that.x;
-		thatY = that.y;
-		
 		double distancePre;
-		distancePre = Math.pow((thatY - thisY), 2) + Math.pow((thatX - thisX), 2);
-		
+		distancePre = Math.pow((that.y - this.y), 2) + Math.pow((that.x - this.x), 2);
 		double distance;
 		distance = Math.sqrt(distancePre);
-		
 		return distance;
 		}
 }
