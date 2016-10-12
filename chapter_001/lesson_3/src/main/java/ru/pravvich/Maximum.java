d9c1ba7ec3ba690487e287cf0f3a3c6007ec01fa
Pavel <triodjangopiter@yandex.ru>
2016-10-12 09:29:57 +0300

    add chapter_001/lesson_3/src/main/java/ru/pravvich/Maximum.java

--- chapter_001/lesson_3/src/main/java/ru/pravvich/Maximum.java
+++ chapter_001/lesson_3/src/main/java/ru/pravvich/Maximum.java
@@ -1,23 +1,23 @@
 package ru.pravvich;
 
 public class Maximum {
- 
     /*
     Вычисляет большую из сторон геометрической фигуры
-    Принимает объекты типа класса Point это точки на двухмерной оси координат, 
-    объекты хранят две координаты по осям "х" и "у".
-    Применяет к ним метод distanceTo для вычисления расстояния между точками из 
-    класса Point.
+    Принимает объекты типа класса Point это точки на двухмерной оси координат, объекты хранят
+    две координаты по осям "х" и "у".
+    Применяет к ним метод distanceTo для вычисления расстояния между точками из класса Point.
     Сравнивает стороны в поисках самой большой и возвращает значение большей стороны
     В случае если треугольник равнобедренный возвращает -1
-    В случае если треугольник равносторонний возвращает -2 
+    В случае если треугольник равносторонний возвращает -2
     */
     public double detectionBiggestSideTriangle(Point a, Point b, Point c) {
 
+        //Вычисляем расстояние между точками
         double distanceAB = a.distanceTo(b);
         double distanceAC = a.distanceTo(c);
         double distanceBC = b.distanceTo(c);
 
+        //Сравниваем вычисленные расстояния
         if (distanceAB > distanceAC && distanceAB > distanceBC) {
             return distanceAB;
         } else if (distanceAC > distanceAB && distanceAC > distanceBC) {
