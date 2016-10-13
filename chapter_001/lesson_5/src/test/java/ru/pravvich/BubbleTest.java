28808dfe67eb85e291b7252c564a6694b1778910
Pavel <triodjangopiter@yandex.ru>
2016-10-13 19:31:46 +0300

    add chapter_001/lesson_5/src/test/java/ru/pravvich/BubbleTest.java

--- chapter_001/lesson_5/src/test/java/ru/pravvich/BubbleTest.java
+++ chapter_001/lesson_5/src/test/java/ru/pravvich/BubbleTest.java
@@ -2,25 +2,25 @@ package ru.pravvich;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class BubbleTest {
     @Test
-    public void whenSetNumberThenCalculateFactorialSingleNumber() {
-        int[] arr = new int[5];
-        arr[0] = 91;
-        arr[1] = 18;
+    public void whenSetNumbersThenSortingFromSmallToLarge() {
+        double[] arr = new double[5];
+        arr[0] = 91.6;
+        arr[1] = 18.123;
         arr[2] = 54;
         arr[3] = 22;
         arr[4] = 14;
         Bubble bubble = new Bubble();
-        int[] result = {14, 18, 22, 54, 91};
+        double[] result = {14, 18.123, 22, 54, 91.6};
         assertThat(result,is(bubble.bubbleSort(arr)));
     }
 
     // Сортировка в обратном порядке
     @Test
-    public void whenSetNumberThenCalculateFactorialSingleNber() {
+    public void whenSetNumbersThenSortingFromLargeToSmall() {
         int[] arr = new int[5];
         arr[0] = 91;
         arr[1] = 18;
