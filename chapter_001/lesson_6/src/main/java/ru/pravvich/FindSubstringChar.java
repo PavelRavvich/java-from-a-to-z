b33ff929ba87df50f55706e84d53cb20d1e60dfb
Pavel <triodjangopiter@yandex.ru>
2016-10-23 04:36:14 +0300

    chapter_001/lesson_6/src/main/java/ru/pravvich/FindSubstringChar.java

---
+++ chapter_001/lesson_6/src/main/java/ru/pravvich/FindSubstringChar.java
@@ -0,0 +1,37 @@
+package ru.pravvich;
+import java.util.Arrays;
+
+/**
+ * Проверяет наличие подстроки в строке
+ * @author Pavel Ravvich 18.10.21
+ * @since 23.10.2016
+ * @author triodjangopiter@yandex.ru
+ * @version 1.0
+ */
+public class FindSubstringChar {
+
+    /**
+     * Проверяет является ли строка sub подстрокой origin
+     * @param origin - строка по которой производится поиск.
+     * @param sub - искомая строка.
+     * <p>
+     * Test:
+     * @see FindSubstringCharTest#WhenSetStringAndSubstringThenFindSubstringInString()
+     * */
+    public boolean contain(String origin, String sub) {
+        char[] arrOrigin = origin.toCharArray();
+        char[] arrSub = sub.toCharArray();
+        boolean result = false;
+        if (arrOrigin.length < arrSub.length) { return false; }
+        for (int i = 0; i <= arrOrigin.length - arrSub.length; i++) {
+            if (arrSub[0] == arrOrigin[i]) {
+                char[] forEqual = new char[arrSub.length];
+                System.arraycopy(arrOrigin, i, forEqual, 0, arrSub.length);
+                if (Arrays.equals(forEqual, arrSub)) {
+                    return true;
+                }
+            }
+        }
+        return result;
+    }
+}
