afb970069d381633e1f39d23b481033ad3ed59db
Pavel <triodjangopiter@yandex.ru>
2016-11-01 20:36:00 +0300

    add chapter_002/tracker/src/main/java/ru/pravvich/models/Item.java

--- chapter_002/tracker/src/main/java/ru/pravvich/models/Item.java
+++ chapter_002/tracker/src/main/java/ru/pravvich/models/Item.java
@@ -1,42 +1,74 @@
 package ru.pravvich.models;
 
+import java.util.ArrayList;
+
 public class Item {
-	public String nameUser;
-	public long create;
-	public String header; 
-	public String description;
-	public int id;
-	
-	public Item() {
+    public String nameUser;
+    public long create;
+    public String header;
+    public String description;
+    public int id;
+    public ArrayList<String> commit = new ArrayList<>();
 
-	}
-	
-	public Item(String nameUser, long create, 
-				String header, String description, int id) {
-		this.nameUser = nameUser;
-		this.create = create;
-		this.header = header;
-		this.description = description;
-		this.id = id;
-	}
+    public Item() {
+    }
 
-	public String getNameUser() {
-		return nameUser;
-	}
+    public Item(String nameUser, long create, String header, 
+                String description, int id, ArrayList<String> commit) {
+        this.nameUser = nameUser;
+        this.create = create;
+        this.header = header;
+        this.description = description;
+        this.id = id;
+        this.commit = commit;
+    }
 
-	public long getCreate() {
-		return create;
-	}
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
 
-	public String getHeader() {
-		return header;
-	}
+    public void setCreate(long create) {
+        this.create = create;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setHeader(String header) {
+        this.header = header;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCommit(ArrayList<String> commit) {
+        this.commit = commit;
+    }
+
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public long getCreate() {
+        return create;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<String> getCommits() {
+        return commit;
+    }
 }
