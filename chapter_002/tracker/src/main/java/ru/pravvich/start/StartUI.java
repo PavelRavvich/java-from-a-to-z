67e5038d8dee0536f33718b6b21517a436024d99
Pavel <triodjangopiter@yandex.ru>
2016-11-01 21:00:58 +0300

    add chapter_002/tracker/src/main/java/ru/pravvich/start/StartUI.java

--- chapter_002/tracker/src/main/java/ru/pravvich/start/StartUI.java
+++ chapter_002/tracker/src/main/java/ru/pravvich/start/StartUI.java
@@ -3,7 +3,7 @@ package ru.pravvich.start;
 import ru.pravvich.models.*;
 
 public class StartUI {
-    public static void main() {
+    public static void main(String[] args) {
         Tracker tracker = new Tracker();
         tracker.add(new Task("Pavel", 25));
     }
