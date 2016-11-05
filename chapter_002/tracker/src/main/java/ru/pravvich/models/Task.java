07a02235c1871273a1b562550c4e336d394c9f28
Pavel <triodjangopiter@yandex.ru>
2016-11-05 08:08:53 +0300

    add chapter_002/tracker/src/main/java/ru/pravvich/models/Task.java

--- chapter_002/tracker/src/main/java/ru/pravvich/models/Task.java
+++ chapter_002/tracker/src/main/java/ru/pravvich/models/Task.java
@@ -3,10 +3,5 @@ package ru.pravvich.models;
 public class Task extends Item {
     public Task(String header ) {
         this.header = header;
-
-    }
-
-    public String calculatePrise() {
-        return "1 000 000";
     }
 }
