6806ca9e233ef2ff33d8f0bd70b606407522ed78
Pavel <triodjangopiter@yandex.ru>
2016-11-22 15:43:01 +0300

    add method add(Figure) in class Board

--- chapter_002/chess-v1.1/src/main/java/ru/pravvich/Board.java
+++ chapter_002/chess-v1.1/src/main/java/ru/pravvich/Board.java
@@ -1,7 +1,14 @@
 package ru.pravvich;
 
-import ru.pravvich.figures.Figure;
+import ru.pravvich.figures.*;
 
 public class Board {
     public static Figure[][] desc = new Figure[8][8];
+
+    private void add(Figure figure) {
+        if (figure instanceof Officer) {
+            desc[figure.getPosition().getY()][figure.getPosition().getX()] =
+                    new Officer(figure.getPosition(), figure.getColor());
+        }
+    }
 }
