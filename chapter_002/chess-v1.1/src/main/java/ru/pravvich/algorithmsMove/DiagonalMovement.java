0d576609714d80262e277e33dff70fc1f90f3f03
Pavel <triodjangopiter@yandex.ru>
2016-11-23 18:03:39 +0300

    Refactoring in class DialgonalMovement method checkRoadRightDown(Figure,Cell). And add refactoring Tests

--- chapter_002/chess-v1.1/src/main/java/ru/pravvich/algorithmsMove/DiagonalMovement.java
+++ chapter_002/chess-v1.1/src/main/java/ru/pravvich/algorithmsMove/DiagonalMovement.java
@@ -1,14 +1,21 @@
 package ru.pravvich.algorithmsMove;
 
-import ru.pravvich.Board;
 import ru.pravvich.Cell;
 import ru.pravvich.ImposableMoveException;
 import ru.pravvich.figures.Figure;
-import ru.pravvich.figures.Place;
 
 public class DiagonalMovement extends Movement {
 
-    private boolean checkRoad = true;
+    @Override
+    public Cell[] move(Figure figure, Cell position) throws ImposableMoveException {
+        if (this.checkDiagonal(figure, position) &&
+                figure.getPosition().getX() < position.getX() &&
+                figure.getPosition().getY() < position.getY()) {
+            return this.checkRoadRightDown(figure, position);
+        } else {
+            throw new ImposableMoveException("Invalid move");
+        }
+    }
 
     // проверяет идет ли фигура ровно по диаганали.
     private boolean checkDiagonal(Figure figure, Cell position) {
@@ -21,40 +28,14 @@ public class DiagonalMovement extends Movement {
         return result;
     }
 
-    // проверяет свободены ли ячейки до цели(не включая ячейку самой цели) и наполняет массив пути.
-    private Cell[] checkRoad(Figure figure, Cell position) {
+    // наполняет массив пути.
+    private Cell[] checkRoadRightDown(Figure figure, Cell position) {
         Cell[] cell = new Cell[position.getY() - figure.getPosition().getY() - 1];
         int index = 0;
         for (int i = figure.getPosition().getY() + 1, j = figure.getPosition().getX() + 1;
              i < position.getY() && j < position.getX(); i++, j++) {
-            cell[index++] = Board.desc[i][j].getPosition();
-            if (!(Board.desc[i][j] instanceof Place)) {
-                this.checkRoad = false;
-            }
+            cell[index++] = new Cell(i, j);
         }
         return cell;
-    }
-
-    @Override
-    public Cell[] move(Figure figure, Cell position) throws ImposableMoveException {
-        if (this.checkDiagonal(figure, position) &&
-                figure.getPosition().getX() < position.getX() &&
-                figure.getPosition().getY() < position.getY()) {
-            return this.moveRightDown(figure, position);
-        } else {
-            throw new ImposableMoveException("Invalid move");
-        }
-    }
-
-    private Cell[] moveRightDown(Figure figure, Cell position) throws ImposableMoveException {
-        Cell[] cell = this.checkRoad(figure, position);
-        if (this.checkRoad && (
-                Board.desc[position.getY()][position.getX()] instanceof Place ||
-                        !figure.getColor().equals(Board.desc[position.getY()][position.getX()].getColor()))
-                ) {
-            return cell;
-        } else {
-            throw new ImposableMoveException("Invalid move!");
-        }
     }
 }
--- chapter_002/chess-v1.1/src/test/java/ru/pravvich/figures/OfficerTest.java
+++ chapter_002/chess-v1.1/src/test/java/ru/pravvich/figures/OfficerTest.java
@@ -1,7 +1,6 @@
 package ru.pravvich.figures;
 
 import org.junit.Test;
-import ru.pravvich.Board;
 import ru.pravvich.Cell;
 import ru.pravvich.ImposableMoveException;
 
@@ -11,30 +10,23 @@ import static org.junit.Assert.assertThat;
 public class OfficerTest {
 
     @Test
-    public void whenRoadGoodThenReturnCellArrWithRoad() throws ImposableMoveException {
+    public void whenThenReturnArrayCellRoadWithoutFinishCell() throws ImposableMoveException {
+        Figure[][] desc = new Figure[8][8];
         for (int i = 0; i != 8; i++) {
-            Board.desc[i][i] = new Place(new Cell(i, i), "");
+            desc[i][i] = new Place(new Cell(i, i), "");
         }
-        Board.desc[4][4] = new Officer(new Cell(4, 4), "white");
-        Cell[] result = Board.desc[4][4].move(new Cell(7, 7));
-        Cell[] check = new Cell[2];
-        check[0] = new Cell(5, 5);
-        check[1] = new Cell(6, 6);
-        assertThat(check[0].getX(), is(result[0].getX()));
-        assertThat(check[0].getY(), is(result[0].getY()));
-        assertThat(check[1].getX(), is(result[1].getX()));
-        assertThat(check[1].getY(), is(result[1].getY()));
-        assertThat(check.length, is(result.length));
+        desc[4][4] = new Officer(new Cell(4, 4), "white");
+        Cell[] result = desc[4][4].move(new Cell(7, 7));
+        assertThat(result.length, is(2));
     }
 
-    @Test
-    public void whenRoadBadThenThrowImposableMoveException() throws ImposableMoveException {
+    public void whenRoadNotImposableForOfficerThenThrowImposableMoveException() throws ImposableMoveException {
+        Figure[][] desc = new Figure[8][8];
         for (int i = 0; i != 8; i++) {
-            Board.desc[i][i] = new Place(new Cell(i, i), "");
+            desc[i][i] = new Place(new Cell(i, i), "");
         }
-        Board.desc[4][4] = new Officer(new Cell(4, 4), "white");
-        Board.desc[6][6] = new Officer(new Cell(6, 6), "white");
-        Cell[] result = Board.desc[4][4].move(new Cell(7, 7));
+        desc[4][4] = new Officer(new Cell(4, 4), "white");
+        Cell[] result = desc[4][4].move(new Cell(6, 7));
         assertThat(result.length, is(0));
     }
 }
