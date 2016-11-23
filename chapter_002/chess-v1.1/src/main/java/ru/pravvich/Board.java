538cd34111ecf29682a14cb831136e62c706e170
Pavel <triodjangopiter@yandex.ru>
2016-11-23 19:52:34 +0300

    refactoring method Board.move(Cell,Cell)

--- chapter_002/chess-v1.1/src/main/java/ru/pravvich/Board.java
+++ chapter_002/chess-v1.1/src/main/java/ru/pravvich/Board.java
@@ -13,29 +13,46 @@ public class Board {
     }
 
     public void move(Cell position, Cell newPosition) throws ImposableMoveException {
-        //
-        boolean existence = false;
-        if (!(desc[position.getY()][position.getX()] instanceof Place)) {
-            existence = true;
-        }
-
-        boolean noFriendFire = false; //color
-        String colorStart = desc[position.getY()][position.getX()].getColor();
-        String colorFinish = desc[newPosition.getY()][newPosition.getX()].getColor();
-        if (!colorStart.equals(colorFinish)) {
-            noFriendFire = true;
-        }
-
-        boolean goodRoad = false;
-        if (desc[position.getY()][position.getX()].move(newPosition).length != 0) {
-            goodRoad = true;
-        }
-
-        if (existence && goodRoad && noFriendFire) {
+        boolean existenceFigure = this.existenceFigure(position, newPosition);
+        boolean checkRoad = this.checkRoad(desc[position.getY()][position.getX()].move(newPosition));
+        boolean noFriendFire = this.noFriendFire(position, newPosition);
+        // если все ок записывает фигуру в новую позицию, а в старую записывает Place
+        if (existenceFigure && checkRoad && noFriendFire) {
             desc[newPosition.getY()][newPosition.getX()] = desc[position.getY()][position.getX()];
             desc[position.getY()][position.getX()] = new Place(new Cell(position.getY(),position.getX()),"");
         } else {
             throw new ImposableMoveException("Error move");
         }
+    }
+
+    // проверяет существует ли фигура
+    public boolean existenceFigure(Cell position, Cell newPosition) {
+        boolean result = false;
+        if (!(desc[position.getY()][position.getX()] instanceof Place)) {
+            result = true;
+        }
+        return result;
+    }
+
+    // проверяет нет ли припядствий на пути
+    private boolean checkRoad(Cell[] addressRoad) {
+        boolean result = true;
+        for (int i = 0; i != addressRoad.length; i++) {
+            if (!(desc[addressRoad[i].getY()][addressRoad[i].getX()] instanceof Place)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    // проверяет не совпадает ли цвет объекта в точке назначения с цветом движущейся фигуры
+    private boolean noFriendFire(Cell position, Cell newPosition) {
+        boolean noFriendFire = false; //check color
+        String colorStart = desc[position.getY()][position.getX()].getColor();
+        String colorFinish = desc[newPosition.getY()][newPosition.getX()].getColor();
+        if (!colorStart.equals(colorFinish)) {
+            noFriendFire = true;
+        }
+        return noFriendFire;
     }
 }
---
+++ chapter_002/chess-v1.1/src/test/java/ru/pravvich/figures/BoardTest.java
@@ -0,0 +1,7 @@
+package ru.pravvich.figures;
+
+/**
+ * Created by pavel on 23.11.16.
+ */
+public class BoardTest {
+}
