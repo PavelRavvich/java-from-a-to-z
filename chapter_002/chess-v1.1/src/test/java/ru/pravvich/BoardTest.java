284af4930424a57883bb6577ad5d195fa87911c7
Pavel <triodjangopiter@yandex.ru>
2016-11-23 19:53:59 +0300

    add BoardTest

---
+++ chapter_002/chess-v1.1/src/test/java/ru/pravvich/BoardTest.java
@@ -0,0 +1,28 @@
+package ru.pravvich;
+
+import org.junit.Test;
+import ru.pravvich.Cell;
+import ru.pravvich.ImposableMoveException;
+import ru.pravvich.figures.Figure;
+import ru.pravvich.figures.Officer;
+import ru.pravvich.figures.Place;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+
+public class BoardTest {
+    @Test
+    public void whenThen() throws ImposableMoveException {
+        for (int i = 0; i != Board.desc.length; i++) {
+            Board.desc[i][i] = new Place(new Cell(i, i), "");
+        }
+        Board board = new Board();
+        Figure officer = new Officer(new Cell(0, 0),"white");
+        Board.desc[0][0] = officer;
+        Cell cell = new Cell(5, 5);
+        board.move(new Cell(0, 0), cell);
+        assertThat(Board.desc[5][5],instanceOf(Officer.class));
+        assertThat(Board.desc[5][5].getColor(),is("white"));
+    }
+}
--- chapter_002/chess-v1.1/src/test/java/ru/pravvich/figures/BoardTest.java
+++
@@ -1,7 +0,0 @@
-package ru.pravvich.figures;
-
-/**
- * Created by pavel on 23.11.16.
- */
-public class BoardTest {
-}
