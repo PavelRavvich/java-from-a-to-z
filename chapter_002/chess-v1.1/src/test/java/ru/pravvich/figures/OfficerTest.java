299f197e53436980aebfd22a305441396b9056af
Pavel <triodjangopiter@yandex.ru>
2016-11-24 01:15:14 +0300

    add BoardTests

--- chapter_002/chess-v1.1/src/test/java/ru/pravvich/figures/OfficerTest.java
+++ chapter_002/chess-v1.1/src/test/java/ru/pravvich/figures/OfficerTest.java
@@ -4,10 +4,24 @@ import org.junit.Test;
 import ru.pravvich.Cell;
 import ru.pravvich.ImposableMoveException;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class OfficerTest {
+    @Test
+    public void whenFigureMoveNotDiagonalThenNothingOccurs() throws ImposableMoveException {
+        Figure[][] desc = new Figure[8][8];
+        for (int i = 0; i != 8; i++) {
+            for (int j = 0; j != desc.length; j++) {
+                desc[i][j] = new Place(new Cell(i, i), "");
+            }
+        }
+        desc[4][4] = new Officer(new Cell(4, 4), "white");
+        desc[4][4].move(new Cell(5, 7));
+        assertThat(desc[4][4].getColor(), is("white"));
+        assertThat(desc[4][7], instanceOf(Place.class));
+    }
 
     @Test
     public void whenThenReturnArrayCellRoadWithoutFinishCell() throws ImposableMoveException {
