090039a249225a798a804cef3e72588cd22c79e6
Pavel <triodjangopiter@yandex.ru>
2016-12-02 10:50:41 +0300

    refactoring lesson_2

--- chapter_003/src/main/java/ru/pravvich/lesson_2/Client.java
+++ chapter_003/src/main/java/ru/pravvich/lesson_2/Client.java
@@ -7,7 +7,7 @@ public class Client {
     public static void main(String[] args) throws IOException {
         Censure censure = new Censure();
         ByteArrayInputStream in = new ByteArrayInputStream(
-                "Не пойму я чем секрет жопа есть а слова нет?".getBytes());
+                "Не пойму я в чем секрет жопа есть а слова нет?".getBytes());
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         censure.dropAbuses(in,out, new String[]{"жопа"});
         System.out.println(out.toString());
