cabac58dfc8f317fc44fd3e99327c85d095f408e
Pavel <triodjangopiter@yandex.ru>
2016-12-09 08:13:54 +0300

    add lesson_6 download file for server

--- chapter_003/src/main/java/ru/pravvich/lesson_6/server/Server.java
+++ chapter_003/src/main/java/ru/pravvich/lesson_6/server/Server.java
@@ -4,19 +4,27 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import static java.lang.String.format;
+
 public class Server {
 
 
     public static void main(String[] args) {
-
     }
 
     ServerSocket serverSocket;
     Socket socket;
 
-    void initSocket() {
+    void initServerSocket() {
         try {
             this.serverSocket = new ServerSocket(5213);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void initSocket() {
+        try {
             this.socket = this.serverSocket.accept();
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,7 +33,6 @@ public class Server {
 
     void download(String nameFile) {
         try (InputStream input = this.socket.getInputStream()){
-            // Саздаём пустой массив байтов из available()
             // available() - возвращает колличество сейчас доступных байтов
             byte[] buffer = new byte[input.available()];
             // читаем
@@ -33,18 +40,21 @@ public class Server {
             while (i != -1) {
                 i = input.read(buffer);
             }
-
-            File file = new File(Paths.REPO.getPath() + "/" + nameFile);
-
-            // Саздаём OutputStream из файла  ...
-            OutputStream outStream = new FileOutputStream(file);
-
-            // Записываем массив байтов в файл
-            outStream.write(buffer);
+            this.createFile(nameFile,buffer);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
+    private boolean createFile(String nameFile, byte[] buffer) {
+        File file = new File(format("%s/%s",Paths.REPO.getPath(),nameFile));
+        if (!file.exists()) {
+            try (OutputStream out = new FileOutputStream(file)) {
+                out.write(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file.exists();
+    }
 }
