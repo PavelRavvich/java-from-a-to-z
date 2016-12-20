75447422788b70c9f52fb899af803a5ff6aced39
Pavel <triodjangopiter@yandex.ru>
2016-12-20 22:58:56 +0300

    refactoring CorrectBracket

--- chapter_003/src/main/java/ru/pravvich/additionalQuestion/CorrectBracket.java
+++ chapter_003/src/main/java/ru/pravvich/additionalQuestion/CorrectBracket.java
@@ -10,25 +10,15 @@ class CorrectBracket {
 
     boolean checkSecond(String bracket) {
         char[] array = bracket.toCharArray();
-
-        if (array.length % 2 != 0)
+        if (array.length % 2 != 0) {
             return false;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == '(') {
-                for (int j = i + 1; j < array.length; j++) {
-                    if (array[j] == ')') {
-                        array[i] = '+';
-                        array[j] = '+';
-                        break;
-                    }
-                }
-            }
         }
-
-        for (char c : array) {
-            if (c != '+')
+        int d = 0;
+        for (char anArray : array) {
+            d += anArray == '(' ? 1 : -1;
+            if (d < 0) {
                 return false;
+            }
         }
         return true;
     }
