7b175d7df12f1b46a5cc26a615e50c5e30891468
Pavel <triodjangopiter@yandex.ru>
2017-04-05 16:40:10 +0300

    synchronized Array Simple Set

--- chapter_005/src/main/java/ru/pravvich/set/arraySimpleSet/ArraySimpleSet.java
+++ chapter_005/src/main/java/ru/pravvich/set/arraySimpleSet/ArraySimpleSet.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class ArraySimpleSet<E> implements SetArray<E> {
 
-    private E[] values;
+    private volatile E[] values;
 
     public ArraySimpleSet() {
         values = (E[]) new Object[0];
@@ -29,7 +29,7 @@ public class ArraySimpleSet<E> implements SetArray<E> {
     }
 
     @Override
-    public boolean add(E e) {
+    public synchronized boolean add(E e) {
         Objects.requireNonNull(e);
         if (notExistDuplicate(e)) {
             try {
